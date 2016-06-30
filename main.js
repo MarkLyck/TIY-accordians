97cacf380cd1290e93dbd2f54134ed68b656bfd2
Mark Lyck <mark.lyck@gmail.com>
2016-06-29 20:32:34 -0500

    Added animations

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var $accords = $('.accord:not(.sign-out)');
+var $ul = $('ul');
 
-$accords.on('click', function(){
+$accords.on('click', function(e){
+  $accords.removeClass('selected');
+  $ul.slideUp('swing');
+  if ($(e.target).next('ul').is(':visible')) {
+    $(e.target).next('ul').slideUp('swing');
+  } else {
+    $(e.target).next('ul').slideDown('swing');
+  }
   $(this).toggleClass('selected');
-  var $ul = $(this).find('ul');
 });
